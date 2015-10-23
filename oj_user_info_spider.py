ac555e7545884b941cb4e9ae60ddf0a54532710a
virusdefender <qduliyang@outlook.com>
2015-10-23 13:17:23 +0800

    完善爬虫；增加生成 report 表格的代码

--- oj_user_info_spider.py
+++ oj_user_info_spider.py
@@ -5,68 +5,57 @@ import datetime
 import re
 import logging
 from thread_pool import ThreadPool
+from db import BaseDBHandler
+
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-class DbHandler(object):
-    def __init__(self):
-        self.db_path = 'D:/result.db'
+class DBHandler(BaseDBHandler):
+    def __init__(self, db_path="result.db"):
+        self.db_path = db_path
+        super(DBHandler, self).__init__(self.db_path)
+        self.init_db()
 
-    def db_user_connection(self):
-        cx = sqlite3.connect(self.db_path)
-        cu = cx.cursor()
-        cu.execute("create table if not exists user (username varchar(20) primary key, "
-                   "bc_name varchar(20) UNIQUE, cf_name varchar(20) UNIQUE, hdu_name varchar(20) UNIQUE)")
-        return cx, cu
+    def init_db(self):
+        self.execute_sql("CREATE TABLE IF NOT EXISTS user (username VARCHAR(20) PRIMARY KEY, "
+                         "bc_name VARCHAR(20) UNIQUE, cf_name VARCHAR(20) UNIQUE, hdu_name VARCHAR(20) UNIQUE)")
 
-    def db_info_connection(self):
-        cx = sqlite3.connect(self.db_path)
-        cu = cx.cursor()
-        cu.execute("create table if not exists info (id integer primary key autoincrement, "
-                   "username varchar(20), bc_rank integer, bc_rating integer, cf_rank integer, hdu_rank integer,"
-                   "hdu_problems_submitted integer, hdu_problems_solved integer, hdu_submissions integer,"
-                   "hdu_accepted integer, crawling_date datetime)")
-        return cx, cu
-
-    def get_user_list(self):
-        cx, cu = self.db_user_connection()
-        cu.execute("select * from user")
-        return cu.fetchall()
+        self.execute_sql("CREATE TABLE IF NOT EXISTS info (id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                         "username VARCHAR(20), bc_rank INTEGER, bc_rating INTEGER, cf_rank INTEGER, hdu_rank INTEGER,"
+                         "hdu_problems_submitted INTEGER, hdu_problems_solved INTEGER, hdu_submissions INTEGER,"
+                         "hdu_accepted INTEGER, crawling_date DATE)")
 
     def save_user_info(self, username, **kwargs):
         bc_result = kwargs["bestcoder"]
         cf_result = kwargs["codeforces"]
         hdu_result = kwargs["hduoj"]
-        cx, cu = self.db_info_connection()
-        now = datetime.datetime.now().strftime('%Y-%m-%d')
-        cu.execute("select * from info where username = ?", (username, ))
-        results = cu.fetchall()
-        # print "results-------------", results[-1][10] != now
-        if not results:
-            cu.execute("insert into info(username, bc_rank, bc_rating, cf_rank, hdu_rank, hdu_problems_submitted,"
-                       "hdu_problems_solved, hdu_submissions, hdu_accepted, crawling_date) "
-                       "values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                       (username, 0, 0, 0, 0, 0, 0, 0, 0, datetime.datetime.now().strftime('%Y-%m-%d')))
-        elif results[-1][10] != now:
-            cu.execute("insert into info(username, bc_rank, bc_rating, cf_rank, hdu_rank, hdu_problems_submitted,"
-                       "hdu_problems_solved, hdu_submissions, hdu_accepted, crawling_date) "
-                       "values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                       (username, 0, 0, 0, 0, 0, 0, 0, 0, datetime.datetime.now().strftime('%Y-%m-%d')))
+
+        today = datetime.date.today()
+
+        # 如果这个人没有今天的爬取记录，就先插入一条全是0的，后面只需要 update 就行了。
+        if not self.execute_sql("SELECT * FROM info WHERE username = ? and crawling_date = ?",
+                                (username, today)).fetchall():
+            self.execute_sql("INSERT INTO info(username, bc_rank, bc_rating, cf_rank, hdu_rank, hdu_problems_submitted,"
+                             "hdu_problems_solved, hdu_submissions, hdu_accepted, crawling_date) "
+                             "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                             (username, 0, 0, 0, 0, 0, 0, 0, 0, today))
+
         if bc_result != {}:
-            cu.execute("update info set bc_rank = ?, bc_rating = ? where username =  ? and crawling_date = ? ",
-                       (bc_result["rank"], bc_result["rating"], username, now))
+            self.execute_sql("UPDATE info SET bc_rank = ?, bc_rating = ? WHERE username =  ? AND crawling_date = ? ",
+                             (bc_result["rank"], bc_result["rating"], username, today))
+            return
         elif cf_result != {}:
-            cu.execute("update info set cf_rank = ? where username =  ? and crawling_date = ? ",
-                       (cf_result["rank"], username, now))
+            self.execute_sql("UPDATE info SET cf_rank = ? WHERE username =  ? AND crawling_date = ? ",
+                             (cf_result["rank"], username, today))
+            return
         else:
-            cu.execute("update info set hdu_rank = ?, hdu_problems_submitted = ?, "
-                       "hdu_problems_solved = ?, hdu_submissions = ?, "
-                       "hdu_accepted = ? where username =  ? and crawling_date = ? ",
-                       (hdu_result["rank"], hdu_result["problems_submitted"],
-                        hdu_result["problems_solved"], hdu_result["submissions"],
-                        hdu_result["accepted"], username, now))
-        cx.commit()
+            self.execute_sql("UPDATE info SET hdu_rank = ?, hdu_problems_submitted = ?, "
+                             "hdu_problems_solved = ?, hdu_submissions = ?, "
+                             "hdu_accepted = ? WHERE username =  ? AND crawling_date = ? ",
+                             (hdu_result["rank"], hdu_result["problems_submitted"],
+                              hdu_result["problems_solved"], hdu_result["submissions"],
+                              hdu_result["accepted"], username, today))
 
 
 class Spider(object):
@@ -74,9 +63,11 @@ class Spider(object):
         user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) " \
                      "Chrome/44.0.2403.130 Safari/537.36"
         headers = {'User-Agent': user_agent}
+        # 重试三次，还不行就抛异常
         for i in range(0, 3):
             try:
                 response = requests.get(url, headers=headers)
+                # 防止403等
                 if response.status_code != 200:
                     raise ValueError()
                 return response.content
@@ -130,7 +121,8 @@ class Spider(object):
             logging.debug("finished hudoj " + hdu_name)
             return {"website": "hduoj",
                     "rank": self.re_search("<td>Rank</td><td align=center>(\d+)</td>", html),
-                    "problems_submitted": self.re_search("<td>Problems Submitted</td><td align=center>(\d+)</td>", html),
+                    "problems_submitted": self.re_search("<td>Problems Submitted</td><td align=center>(\d+)</td>",
+                                                         html),
                     "problems_solved": self.re_search("<td>Problems Solved</td><td align=center>(\d+)</td>", html),
                     "submissions": self.re_search("<td>Submissions</td><td align=center>(\d+)</td>", html),
                     "accepted": self.re_search("<td>Accepted</td><td align=center>(\d+)</td>", html)}
@@ -142,14 +134,11 @@ class Spider(object):
                     "accepted": 0}
 
 
-USERNAME = "hohoTT"
-BC_USERNAME = "KirinoP"
-CF_USERNAME = "tourist"
-HDU_USERNAME = "bluebird"
-
-d = DbHandler()
+d = DBHandler()
 s = Spider()
+
 user_list = d.get_user_list()
+
 pool = ThreadPool(size=10)
 pool.start()
 
@@ -166,10 +155,11 @@ for user in user_list:
     pool.append_job(add_username, s.hduoj, user[0], user[3])
 pool.join()
 pool.stop()
+
+
 while not pool.results.empty():
     result = pool.results.get()
     username = result["username"]
-
     kwargs = {"bestcoder": {}, "codeforces": {}, "hduoj": {}}
     kwargs[result["website"]] = result
 
---
+++ report_generator.py
@@ -0,0 +1,94 @@
+# coding=utf-8
+import datetime
+from db import BaseDBHandler
+
+
+class ReportGenerator(BaseDBHandler):
+    def __init__(self, db_path='result.db'):
+        super(ReportGenerator, self).__init__(db_path)
+
+    def fetch(self):
+        dates = self.execute_sql("SELECT DISTINCT crawling_date FROM info ORDER BY crawling_date ASC").fetchall()
+        users = self.get_user_list()
+
+        results = []
+
+        for user in users:
+            tmp = []
+            for date in dates:
+                date = datetime.datetime.strptime(date[0], "%Y-%m-%d").date()
+                r = self.execute_sql("SELECT * FROM info WHERE username = ? AND crawling_date = ?",
+                                     (user[0], date)).fetchall()
+                if r:
+                    tmp.append(r[0])
+                else:
+                    tmp.append((-1, u'----', 0, 0, 0, 0, 0, 0, 0, 0, u'----'))
+            results.append(tmp)
+        return {"dates": dates, "users": users, "results": results}
+
+    def _generate_thead(self, dates):
+        html = u"<thead><tr><th>username</th><th>type</th>"
+        for item in dates:
+            html += (u"<th>" + str(item[0]) + u"</th>")
+        html += u"</tr></thead>"
+        return html
+
+    def _generate_tbody(self, results):
+        html = u"<tbody>"
+
+        for item in results:
+            html += u"<tr class=\"warning\"><td rowspan=\"8\">" + item[0][1] + u"</td>"
+            for i in range(2, 10):
+                if i == 2:
+                    t = ""
+                elif i == 3:
+                    t = u"<tr class=\"warning\">"
+                elif i == 4:
+                    t = u"<tr class=\"success\">"
+                else:
+                    t = u"<tr class=\"info\">"
+                t += u"<td>" + \
+                     ["BestCoder_rank", "BestCoder Rating", "CodeForces Rank", "hduoj rank",
+                      "hduoj problems submitted", "hduoj problems solved",
+                      "hduoj submissions", "hduoj accepted"][i - 2] \
+                     + u"</td>"
+                for day_results in item:
+                    t += u"<td>" + str(day_results[2]) + u"</td>"
+                t += u"</tr>"
+                html += t
+        html += u"</tbody>"
+        return html
+
+    def generate_report(self):
+        html = u"""<!DOCTYPE html>
+            <html lang="zh-cn">
+            <head>
+                <meta charset="UTF-8">
+                <title></title>
+                <!-- 新 Bootstrap 核心 CSS 文件 -->
+                <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css">
+                <style>
+                body{
+                    margin: 30px;
+                }
+                </style>
+
+            </head>
+            <body>
+                <div>
+                <table class="table table-bordered">
+        """
+        data = self.fetch()
+        html += self._generate_thead(data["dates"])
+        html += self._generate_tbody(data["results"])
+        html += u"</table></div></html>"
+        return html
+
+    def save(self):
+        report = self.generate_report()
+        f = open("report.html", "w")
+        f.write(report.encode("utf-8"))
+        f.close()
+
+g = ReportGenerator()
+g.save()
